33dd92c66d9bbfa1b26ad6750e6edd15b87ed04b
blue undefined <isakov.t.a@edu.mirea.ru>
2024-06-14 03:08:13 +0300

    Update cadastral_manual.py

--- app/models/cadastral_manual.py
+++ app/models/cadastral_manual.py
@@ -4,24 +4,76 @@ from typing import Optional, List
 from pydantic import BaseModel, Field
 
 class CadastralManualBase(BaseModel):
-    cadastral_quarter_number: str = Field(..., alias="Номер кадастрового квартала")
-    land_for_high_rise_residential_buildings: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения объектов многоэтажной жилой застройки")
-    land_for_low_rise_residential_buildings: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения малоэтажной жилой застройки, включая индивидуальную жилую застройку")
-    land_for_garages_parking: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения гаражей, машино-мест, автостоянок")
-    land_for_commerce_recreation: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения объектов торговли, общественного питания, бытового обслуживания, сервиса, отдыха и развлечений, включая объекты многофункционального назначения")
-    land_for_temporary_residence: Optional[float] = Field(None, alias="Земельные участки под объектами, предназначенными для временного проживания")
-    land_for_office_buildings: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения административных и офисных зданий")
-    land_for_industrial_purposes: Optional[float] = Field(None, alias="Земельные участки производственного назначения")
-    land_for_sanatoriums_tourism: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения санаториев и объектов туристического назначения")
-    land_for_social_infrastructure: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения объектов социальной инфраструктуры")
-    land_for_ports_stations: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения объектов портов, вокзалов, станций")
-    land_for_gardening: Optional[float] = Field(None, alias="Земельные участки, предназначенные для размещения объектов садоводства и огородничества")
+    cadastral_quarter_number: str = Field(
+        ..., 
+        alias="cadastral_quarter_number", 
+        description="Номер кадастрового квартала"
+    )
+    high_rise_residential: Optional[float] = Field(
+        None, 
+        alias="high_rise_residential", 
+        description="Земельные участки, предназначенные для размещения объектов многоэтажной жилой застройки"
+    )
+    low_rise_residential: Optional[float] = Field(
+        None, 
+        alias="low_rise_residential", 
+        description="Земельные участки, предназначенные для размещения малоэтажной жилой застройки, включая индивидуальную жилую застройку"
+    )
+    garages_parking: Optional[float] = Field(
+        None, 
+        alias="garages_parking", 
+        description="Земельные участки, предназначенные для размещения гаражей, машино-мест, автостоянок"
+    )
+    commerce_recreation: Optional[float] = Field(
+        None, 
+        alias="commerce_recreation", 
+        description="Земельные участки, предназначенные для размещения объектов торговли, общественного питания, бытового обслуживания, сервиса, отдыха и развлечений, включая объекты многофункционального назначения"
+    )
+    temporary_residence: Optional[float] = Field(
+        None, 
+        alias="temporary_residence", 
+        description="Земельные участки под объектами, предназначенными для временного проживания"
+    )
+    office_buildings: Optional[float] = Field(
+        None, 
+        alias="office_buildings", 
+        description="Земельные участки, предназначенные для размещения административных и офисных зданий"
+    )
+    industrial_purposes: Optional[float] = Field(
+        None, 
+        alias="industrial_purposes", 
+        description="Земельные участки производственного назначения"
+    )
+    sanatoriums_tourism: Optional[float] = Field(
+        None, 
+        alias="sanatoriums_tourism", 
+        description="Земельные участки, предназначенные для размещения санаториев и объектов туристического назначения"
+    )
+    social_infrastructure: Optional[float] = Field(
+        None, 
+        alias="social_infrastructure", 
+        description="Земельные участки, предназначенные для размещения объектов социальной инфраструктуры"
+    )
+    ports_stations: Optional[float] = Field(
+        None, 
+        alias="ports_stations", 
+        description="Земельные участки, предназначенные для размещения объектов портов, вокзалов, станций"
+    )
+    gardening: Optional[float] = Field(
+        None, 
+        alias="gardening", 
+        description="Земельные участки, предназначенные для размещения объектов садоводства и огородничества"
+    )
 
 class CadastralManualCreate(CadastralManualBase):
     pass
 
 class CadastralManualGet(CadastralManualBase):
-    id: int = Field(..., alias="Уникальный идентификатор")
+    id: int = Field(
+        ..., 
+        alias="id", 
+        description="Уникальный идентификатор"
+    )
 
     class Config:
         from_attributes = True
